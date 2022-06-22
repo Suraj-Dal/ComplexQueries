99bfc7975f5e1b0fcfb76d43ba99fcd071f8f4b9
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-22 22:51:09 +0530

    [Suraj]: Usecase 3 to Join the two table using different types of joins is added.

--- University.sql
+++ University.sql
@@ -24,6 +24,8 @@ INSERT INTO Student(Name,Contact,Department,Year) VALUES
 ('Mayuresh','9049091633','Computer','2021'),
 ('Saket','9046634554','Electrical','2020');
 SELECT * FROM Student;
+INSERT INTO Student(Name,Contact,Department,Year) VALUES
+('Sandip','9049876541','Mechanical','2017');
 
 INSERT INTO Subject(ID,Subject_Name,Semester) VALUES
 ('1','Advance Java','Fourth'),
@@ -32,5 +34,35 @@ INSERT INTO Subject(ID,Subject_Name,Semester) VALUES
 ('4','Basic Electrical','Second'),
 ('3','CPP','Second');
 SELECT * FROM Subject;
+INSERT INTO Subject(ID,Subject_Name,Semester) VALUES
+('5','Building Material','Third'),
+('2','Advance Electrical','Fifth');
+
+--Inner Join
+SELECT ST.Name, SB.Subject_Name
+FROM Student ST JOIN Subject SB
+ON ST.ID = SB.ID;
+
+--Left Join
+SELECT ST.Name, SB.Subject_Name
+FROM Student ST LEFT JOIN Subject SB
+ON ST.ID = SB.ID;
+
+--Right Join
+SELECT ST.Name, SB.Subject_Name
+FROM Student ST RIGHT JOIN Subject SB
+ON ST.ID = SB.ID;
+
+--Full Join
+SELECT ST.Name, SB.Subject_Name
+FROM Student ST FULL JOIN Subject SB
+ON ST.ID = SB.ID;
+
+--Cross Join
+SELECT ST.Name, SB.Subject_Name
+FROM Student ST CROSS JOIN Subject SB;
+
+
+
 
 
