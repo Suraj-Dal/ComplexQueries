496bb2df3a220be8d033cb98cdc89c78d3567e69
Suraj-Dal <dsurajdal10@gmail.com>
2022-06-22 22:33:32 +0530

    [Suraj]: Usecase 2to insert data into table is added.

--- University.sql
+++ University.sql
@@ -8,10 +8,29 @@ Name varchar(30) not null,
 Contact varchar(30) not null,
 Department varchar(30) not null,
 Year varchar(10) not null);
+SELECT * FROM Student;
 
 CREATE TABLE Subject(
 ID int not null,
 Subject_Name varchar(30) not null,
 Semester varchar(30) not null
 );
+SELECT * FROM Subject;
+
+--Insert Records into table
+INSERT INTO Student(Name,Contact,Department,Year) VALUES
+('Suraj','9049091231','Computer','2021'),
+('Sanket','9087561231','Civil','2018'),
+('Mayuresh','9049091633','Computer','2021'),
+('Saket','9046634554','Electrical','2020');
+SELECT * FROM Student;
+
+INSERT INTO Subject(ID,Subject_Name,Semester) VALUES
+('1','Advance Java','Fourth'),
+('2','Construction Tech','Second'),
+('1','Data Structure','Third'),
+('4','Basic Electrical','Second'),
+('3','CPP','Second');
+SELECT * FROM Subject;
+
 
